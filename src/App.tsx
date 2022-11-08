2cc9b808e134aff05ce5cc3b7325969c3a67bddd
Anastasia951 <n.ivanova504@gmail.com>
2022-11-08 23:15:05 +0300

    correct initial files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { FullTicket } from './pages/FullTicket/FullTicket'
+import { Home } from './pages/Home/Home'
+import { NotFound } from './pages/NotFound/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/full/:ticketId' element={<FullTicket />}></Route>
+      <Route path='*' element={<NotFound />}></Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App

e1a8a88df42f74e90f014745ea25f0a2dc8de80e
Anastasia951 <n.ivanova504@gmail.com>
2022-11-08 23:14:34 +0300

    create Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import cn from 'classnames'
 
+import { Filters } from '../../components/Filters/Filters'
+import styles from './Home.module.scss'
+import { Column } from '../../components/Column/Column'
 export const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className={cn(styles.container)}>
+      <Filters />
+      <div className={cn(styles.columns)}>
+        <Column title='Todo' />
+        <Column title='In Progress' />
+        <Column title='Done' />
+      </div>
+    </div>
+  )
 }
